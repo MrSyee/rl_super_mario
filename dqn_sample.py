0d05e224e1429173284db613ce5c2aebc6a1e769
MrSyee <blue2856@naver.com>
2017-12-27 21:19:00 +0900

    dqn_sample indent

--- dqn_sample.py
+++ dqn_sample.py
@@ -66,112 +66,112 @@ class DQNAgent:
             self.model.load_weights("./save_model/supermario_dqn.h5")
 
         # 상태가 입력, 큐함수가 출력인 인공신경망 생성
-        def build_model(self):
-            model = Sequential() # conv filter output size = (N-F/Stride) + 1
-            model.add(Conv2D(32, (8, 8), strides=(4, 4), activation='relu',   # 84x84 -> 20x20 (padding=valid(default) or same)
-                             input_shape=self.state_size))
-            model.add(Conv2D(64, (4, 4), strides=(2, 2), activation='relu'))  # 20x20 -> 9x9
-            model.add(Conv2D(64, (3, 3), strides=(1, 1), activation='relu'))  # 9x9 -> 7x7
-            model.add(Flatten())
-            model.add(Dense(512, activation='relu')) # 7x7x64= 3136 -> 512
-            model.add(Dense(self.n_action))       # 512 -> n_action
-            model.summary()
-            return model
+    def build_model(self):
+        model = Sequential() # conv filter output size = (N-F/Stride) + 1
+        model.add(Conv2D(32, (8, 8), strides=(4, 4), activation='relu',   # 84x84 -> 20x20 (padding=valid(default) or same)
+                         input_shape=self.state_size))
+        model.add(Conv2D(64, (4, 4), strides=(2, 2), activation='relu'))  # 20x20 -> 9x9
+        model.add(Conv2D(64, (3, 3), strides=(1, 1), activation='relu'))  # 9x9 -> 7x7
+        model.add(Flatten())
+        model.add(Dense(512, activation='relu')) # 7x7x64= 3136 -> 512
+        model.add(Dense(self.n_action))       # 512 -> n_action
+        model.summary()
+        return model
 
-        # 학습된 신경망을 불러옴
-        def load_model(self, filename):
-            self.model.load_weights(filename)
+    # 학습된 신경망을 불러옴
+    def load_model(self, filename):
+        self.model.load_weights(filename)
 
-        # 타겟 모델을 모델의 가중치로 업데이트
-        def update_target_model(self):
-            self.target_model.set_weights(self.model.get_weights())
+    # 타겟 모델을 모델의 가중치로 업데이트
+    def update_target_model(self):
+        self.target_model.set_weights(self.model.get_weights())
 
-        # select action
-        def get_action(self, history):
-            if np.random.rand() <= self.epsilon:
-                return random.randrange(self.n_action)
+    # select action
+    def get_action(self, history):
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.n_action)
+        else:
+            q_value = self.model.predict(history)
+            return np.argmax(q_value[0])
+
+    # 샘플 <s, a, r, s'>을 리플레이 메모리에 저장
+    def append_sample(self, history, action, reward, next_history, dead):
+        self.memory.append((history, action, reward, next_history, dead))
+
+    # 리플레이 메모리에서 무작위로 추출한 배치로 모델 학습
+    def train_model(self):
+        if self.epsilon > self.epsilon_end:
+            self.epsilon -= self.epsilon_decay_step
+
+        mini_batch = random.sample(self.memory, self.batch_size) # replay memory에서 batch_size만큼 뽑아옴
+
+        history = np.zeros((self.batch_size, self.state_size[0],      # [batch_size(32), 84, 84, 4]
+                            self.state_size[1], self.state_size[2]))
+        next_history = np.zeros((self.batch_size, self.state_size[0], # [batch_size(32), 84, 84, 4]
+                                 self.state_size[1], self.state_size[2]))
+        target = np.zeros((self.batch_size,))
+        action, reward, dead = [], [], []
+
+        for i in range(self.batch_size):
+            history[i] = np.float32(mini_batch[i][0])
+            next_history[i] = np.float32(mini_batch[i][3])
+            action.append(mini_batch[i][1])
+            reward.append(mini_batch[i][2])
+            dead.append(mini_batch[i][4])
+
+        target_value = self.target_model.predict(next_history)
+
+        for i in range(self.batch_size):
+            if dead[i]:
+                target[i] = reward[i]
             else:
-                q_value = self.model.predict(history)
-                return np.argmax(q_value[0])
+                target[i] = reward[i] + self.discount_factor * \
+                                        np.amax(target_value[i])
 
-        # 샘플 <s, a, r, s'>을 리플레이 메모리에 저장
-        def append_sample(self, history, action, reward, next_history, dead):
-            self.memory.append((history, action, reward, next_history, dead))
+        loss = self.optimizer([history, action, target])
+        self.avg_loss += loss[0]
 
-        # 리플레이 메모리에서 무작위로 추출한 배치로 모델 학습
-        def train_model(self):
-            if self.epsilon > self.epsilon_end:
-                self.epsilon -= self.epsilon_decay_step
+    # Huber Loss를 이용하기 위해 최적화 함수를 직접 정의
+    def optimizer(self):
+        a = K.placeholder(shape=(None,), dtype='int32')
+        y = K.placeholder(shape=(None,), dtype='float32')
 
-            mini_batch = random.sample(self.memory, self.batch_size) # replay memory에서 batch_size만큼 뽑아옴
+        prediction = self.model.output
 
-            history = np.zeros((self.batch_size, self.state_size[0],      # [batch_size(32), 84, 84, 4]
-                                self.state_size[1], self.state_size[2]))
-            next_history = np.zeros((self.batch_size, self.state_size[0], # [batch_size(32), 84, 84, 4]
-                                     self.state_size[1], self.state_size[2]))
-            target = np.zeros((self.batch_size,))
-            action, reward, dead = [], [], []
+        a_one_hot = K.one_hot(a, self.n_action)
+        q_value = K.sum(prediction * a_one_hot, axis=1)
+        error = K.abs(y - q_value)
 
-            for i in range(self.batch_size):
-                history[i] = np.float32(mini_batch[i][0])
-                next_history[i] = np.float32(mini_batch[i][3])
-                action.append(mini_batch[i][1])
-                reward.append(mini_batch[i][2])
-                dead.append(mini_batch[i][4])
+        quadratic_part = K.clip(error, 0.0, 1.0)
+        linear_part = error - quadratic_part
+        loss = K.mean(0.5 * K.square(quadratic_part) + linear_part)
 
-            target_value = self.target_model.predict(next_history)
+        optimizer = RMSprop(lr=0.00025, epsilon=0.01)
+        updates = optimizer.get_updates(self.model.trainable_weights, [], loss)
+        train = K.function([self.model.input, a, y], [loss], updates=updates)
 
-            for i in range(self.batch_size):
-                if dead[i]:
-                    target[i] = reward[i]
-                else:
-                    target[i] = reward[i] + self.discount_factor * \
-                                            np.amax(target_value[i])
+        return train
 
-            loss = self.optimizer([history, action, target])
-            self.avg_loss += loss[0]
+    # 각 에피소드 당 학습 정보를 기록
+    def setup_summary(self):
+        episode_total_reward = tf.Variable(0.)
+        episode_avg_max_q = tf.Variable(0.)
+        episode_duration = tf.Variable(0.)
+        episode_avg_loss = tf.Variable(0.)
 
-        # Huber Loss를 이용하기 위해 최적화 함수를 직접 정의
-        def optimizer(self):
-            a = K.placeholder(shape=(None,), dtype='int32')
-            y = K.placeholder(shape=(None,), dtype='float32')
+        tf.summary.scalar('Total Reward/Episode', episode_total_reward)
+        tf.summary.scalar('Average Max Q/Episode', episode_avg_max_q)
+        tf.summary.scalar('Duration/Episode', episode_duration)
+        tf.summary.scalar('Average Loss/Episode', episode_avg_loss)
 
-            prediction = self.model.output
-
-            a_one_hot = K.one_hot(a, self.n_action)
-            q_value = K.sum(prediction * a_one_hot, axis=1)
-            error = K.abs(y - q_value)
-
-            quadratic_part = K.clip(error, 0.0, 1.0)
-            linear_part = error - quadratic_part
-            loss = K.mean(0.5 * K.square(quadratic_part) + linear_part)
-
-            optimizer = RMSprop(lr=0.00025, epsilon=0.01)
-            updates = optimizer.get_updates(self.model.trainable_weights, [], loss)
-            train = K.function([self.model.input, a, y], [loss], updates=updates)
-
-            return train
-
-        # 각 에피소드 당 학습 정보를 기록
-        def setup_summary(self):
-            episode_total_reward = tf.Variable(0.)
-            episode_avg_max_q = tf.Variable(0.)
-            episode_duration = tf.Variable(0.)
-            episode_avg_loss = tf.Variable(0.)
-
-            tf.summary.scalar('Total Reward/Episode', episode_total_reward)
-            tf.summary.scalar('Average Max Q/Episode', episode_avg_max_q)
-            tf.summary.scalar('Duration/Episode', episode_duration)
-            tf.summary.scalar('Average Loss/Episode', episode_avg_loss)
-
-            summary_vars = [episode_total_reward, episode_avg_max_q,
-                            episode_duration, episode_avg_loss]
-            summary_placeholders = [tf.placeholder(tf.float32) for _ in
-                                    range(len(summary_vars))]
-            update_ops = [summary_vars[i].assign(summary_placeholders[i]) for i in
-                          range(len(summary_vars))]
-            summary_op = tf.summary.merge_all()
-            return summary_placeholders, update_ops, summary_op
+        summary_vars = [episode_total_reward, episode_avg_max_q,
+                        episode_duration, episode_avg_loss]
+        summary_placeholders = [tf.placeholder(tf.float32) for _ in
+                                range(len(summary_vars))]
+        update_ops = [summary_vars[i].assign(summary_placeholders[i]) for i in
+                      range(len(summary_vars))]
+        summary_op = tf.summary.merge_all()
+        return summary_placeholders, update_ops, summary_op
 
 if __name__ == "__main__":
     # 환경과 DQN 에이전트 생성
